ab0a6fb35d0413904831bc44cfd4aeb5544e1329
Sean Zheng <blackhorseya@gmail.com>
2022-12-15 12:22:28 +0900

    feat: add internal pkg
    - httpx
    - config
    - log

---
+++ internal/pkg/httpx/client.go
@@ -0,0 +1,22 @@
+package httpx
+
+import (
+	"net/http"
+
+	"github.com/blackhorseya/portto/pkg/httpx"
+	"github.com/google/wire"
+)
+
+type client struct {
+}
+
+func NewClient() httpx.Client {
+	return &client{}
+}
+
+func (i *client) Do(req *http.Request) (resp *http.Response, err error) {
+	return http.DefaultClient.Do(req)
+}
+
+// ProviderClientSet is a provider set for http client
+var ProviderClientSet = wire.NewSet(NewClient)
