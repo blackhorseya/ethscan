6ded38b80e59de0237f488620dc30dc98e8c6c53
Sean Zheng <blackhorseya@gmail.com>
2022-12-15 13:01:29 +0900

    feat: add activity biz interface

---
+++ pkg/entity/domain/block/biz/interface.go
@@ -0,0 +1,23 @@
+package biz
+
+import (
+	"github.com/blackhorseya/portto/pkg/contextx"
+	bm "github.com/blackhorseya/portto/pkg/entity/domain/block/model"
+)
+
+// ListCondition declare list block record condition
+type ListCondition struct {
+	Page int `json:"page"`
+	Size int `json:"size"`
+}
+
+// IBiz declare block domain interface
+//
+//go:generate mockery --all --inpackage
+type IBiz interface {
+	// GetByHash serve caller to given hash to get block record
+	GetByHash(ctx contextx.Contextx, hash string) (record *bm.BlockRecord, err error)
+
+	// List serve caller to given condition to get block records
+	List(ctx contextx.Contextx, cond ListCondition) (records []*bm.BlockRecord, err error)
+}
