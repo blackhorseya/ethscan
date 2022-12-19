2553657bf2678f975f1705e21a9bcf5b2c4e7dc1
Sean Zheng <blackhorseya@gmail.com>
2022-12-19 14:36:18 +0900

    fix: update records schema

--- scripts/migrations/block/000001_add_records_table.up.sql
+++ scripts/migrations/block/000001_add_records_table.up.sql
@@ -1,11 +1,10 @@
 create table if not exists records
 (
-    id          bigint                                                                                    not null,
     height      bigint                                                                                    not null comment 'height of block',
     hash        char(66)     default '0x0000000000000000000000000000000000000000000000000000000000000000' not null comment 'hash of block',
     parent_hash char(66)     default '0x0000000000000000000000000000000000000000000000000000000000000000' not null comment 'hash of parent block',
     timestamp   timestamp(6) default 0                                                                    not null comment 'timestamp',
-    constraint records_pk primary key (id)
+    constraint records_pk primary key (hash)
 ) engine = InnoDB
   default charset = utf8mb4
   collate = utf8mb4_general_ci
