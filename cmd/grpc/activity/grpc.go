902e009ec0c22324d5959ad013195743396869d3
Sean Zheng <blackhorseya@gmail.com>
2022-12-30 08:34:22 +0800

    feat: impl activity grpc

--- cmd/grpc/activity/grpc.go
+++ cmd/grpc/activity/grpc.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 
+	"github.com/blackhorseya/ethscan/internal/pkg/errorx"
 	"github.com/blackhorseya/ethscan/pkg/adapters"
+	"github.com/blackhorseya/ethscan/pkg/contextx"
 	ab "github.com/blackhorseya/ethscan/pkg/entity/domain/activity/biz"
 	am "github.com/blackhorseya/ethscan/pkg/entity/domain/activity/model"
 	"go.uber.org/zap"
@@ -28,7 +30,22 @@ func (i *impl) RegisterServer() error {
 	return nil
 }
 
-func (i *impl) ListTxnsByBlockHash(ctx context.Context, req *am.ListTxnsByBlockHashRequest) (*am.ListTxnsByBlockHashResponse, error) {
-	// todo: 2022/12/30|sean|impl me
-	panic("implement me")
+func (i *impl) ListTxnsByBlockHash(c context.Context, req *am.ListTxnsByBlockHashRequest) (*am.ListTxnsByBlockHashResponse, error) {
+	ctx, ok := c.Value(contextx.KeyCtx).(contextx.Contextx)
+	if !ok {
+		return nil, errorx.ErrContextx
+	}
+
+	condition := ab.ListTxnsCondition{
+		BlockHash: req.Hash,
+	}
+	ret, err := i.biz.ListTxns(ctx, condition)
+	if err != nil {
+		ctx.Error(errorx.ErrGetTx.LogMessage, zap.Error(err), zap.Any("condition", condition))
+		return nil, err
+	}
+
+	return &am.ListTxnsByBlockHashResponse{
+		Transactions: ret,
+	}, nil
 }
