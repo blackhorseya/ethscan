f32923b1ffff272923060b92cb7c880ca9380361
Sean Zheng <blackhorseya@gmail.com>
2023-01-15 08:15:49 +0800

    ci: fix go.test.sh

--- scripts/go.test.sh
+++ scripts/go.test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" >coverage.txt
 
-for d in $(go list ./... | grep -v pb | grep -v docs | grep -v mock); do
+for d in $(go list ./... | grep -v pb | grep -v docs | grep -v mock | grep -v gen); do
   go test -race -coverprofile=profile.out -covermode=atomic --tags=unit "$d"
   if [ -f profile.out ]; then
     cat profile.out | grep -v "mock" >>coverage.txt
